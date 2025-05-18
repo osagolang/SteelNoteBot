c8c4601acbf9fc30787c253293eac725a20a68a3
osagolang <lovimtutbait@yandex.ru>
2025-05-18 21:49:22 +0300

    Добавил ввод данных о тренировке + вывод инфо о последних тренировках

--- internal/telegram/handler_exercise_selected.go
+++ internal/telegram/handler_exercise_selected.go
@@ -1,1 +1,34 @@
 package telegram
+
+import (
+	"context"
+	"fmt"
+)
+
+func (h *Handler) HandleExerciseSelected(chatID int64, exerciseID int) {
+
+	records, err := h.recordSVC.GetRecords(context.Background(), chatID, exerciseID, 5)
+	if err != nil {
+		h.HandleSendMessage(chatID, "Ошибка получения тренировок", nil)
+		return
+	}
+
+	msg := ""
+
+	if len(records) == 0 {
+		msg = "Упс! Ты ещё ничего не записывал\n"
+	} else {
+		msg = "Последние тренировки:\n\n"
+
+		for _, r := range records {
+			msg += fmt.Sprintf("%s - %.1f кг. x %d раз(а)\n", r.CreatedAt.Format("02.01"), r.Weight, r.Reps)
+		}
+	}
+
+	msg += "\n---\nВведи данные текущей тренировки\nв формате: вес повторения (например: 90 10)"
+
+	h.tempInput[chatID] = exerciseID
+
+	h.HandleSendMessage(chatID, msg, nil)
+
+}
--- internal/telegram/handler_message.go
+++ internal/telegram/handler_message.go
@@ -1,6 +1,13 @@
 package telegram
 
-import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+import (
+	"context"
+	"fmt"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"log"
+	"strconv"
+	"strings"
+)
 
 func (h *Handler) HandleSendMessage(chatID int64, text string, replyMarkup interface{}) {
 	msg := tgbotapi.NewMessage(chatID, text)
@@ -13,4 +20,40 @@ func (h *Handler) HandleSendMessage(chatID int64, text string, replyMarkup inter
 func (h *Handler) HandleDeleteMessage(chatID int64, messageID int) {
 	deleteMsg := tgbotapi.NewDeleteMessage(chatID, messageID)
 	h.bot.Send(deleteMsg)
+}
+
+func (h *Handler) HandleTrainingMessage(msg *tgbotapi.Message) error {
+	chatID := msg.Chat.ID
+	text := msg.Text
+
+	exerciseID, ok := h.tempInput[chatID]
+	if !ok {
+		h.HandleSendMessage(chatID, "Надо выбрать упражнение", nil)
+		return nil
+	}
+
+	parts := strings.Fields(text)
+	if len(parts) != 2 {
+		h.HandleSendMessage(chatID, "Неверный формат.\nВведи: вес повторения\nНапример: 100 5", nil)
+		return nil
+	}
+
+	weight, err1 := strconv.ParseFloat(parts[0], 64)
+	reps, err2 := strconv.Atoi(parts[1])
+	if err1 != nil || err2 != nil {
+		h.HandleSendMessage(chatID, "Неверный формат. Введи: вес повторения\nНапример: 100 5", nil)
+		return nil
+	}
+
+	err := h.recordSVC.AddRecord(context.Background(), chatID, exerciseID, weight, reps)
+	if err != nil {
+		log.Printf("Ошибка при сохранении тренировки: %v", err)
+		h.HandleSendMessage(chatID, "Ошибка сохранения тренировки", nil)
+		return nil
+	}
+
+	delete(h.tempInput, chatID)
+
+	h.HandleSendMessage(chatID, fmt.Sprintf("✅ Тренировка сохранена: %.1f кг. × %d раз", weight, reps), nil)
+	return nil
 }
